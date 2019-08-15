4855d46f8e7daefc7abef692e02636e111f66d4c
unknown <ThisIsMattSaunders>
2019-08-15 23:25:04 +1200

    I just want to go to sleep, please push

--- js/script.js
+++ js/script.js
@@ -1,16 +1,55 @@
+let newsBox = document.getElementById('newsBox');
+  console.log(newsBox);
+$('#storyButton').click(function(){
 
+    var key;
+    $.ajax({
+        url: "config.json",
+        dataType: "json",
+        type: "GET",
+        success:function(data){
+            key = data[0].NEWS_API_KEY; //key from config.json
+            console.log(key);
+            getData(key);
+        },
+        error:function(error){
+            console.log("ERROR");
+            console.log(error);
+        }
 
-
-// fetching headline data
-var url = 'https://newsapi.org/v2/top-headlines?' +
-          'country=nz&' +
-          'apiKey=';
-var req = new Request(url);
-fetch(req)
-    .then(function(response) {
-        console.log(response.json());
     });
 
-$('#storyButton').click(function(){
-    $('h1').hide();
-});
+
+    function getData(key){
+        console.log(key);
+        $.ajax({
+            url: 'https://newsapi.org/v2/top-headlines?' +
+              'country=nz&' +
+              'apiKey='+ key,
+            dataType: "json",
+            type: "GET",
+            success:function(newsData){
+                dataJSON = newsData;
+                console.log(dataJSON);
+                // console.log(newsData.articles.length);
+                for (var i = 0; i < newsData.articles.length; i++) {
+                    // console.log(newsData.articles[i].title, newsData.articles[i].description);
+                    // newsBox.innerHTML += newsData.articles[i].title, newsData.articles[i].description;
+                    // Use bootstrap cards to add image, title, description, link
+                    newsBox.innerHTML +=
+                    '<div class="card" style="width: 18rem;">' +
+                          '<div class="card-body">' +
+                            '<h5 class="card-title">'+newsData.articles[i].title+'</h5>'+
+                            '<p class="card-text">'+newsData.articles[i].description+'</p>'+
+                            '<a href="'+newsData.articles[i].url+'" class="btn btn-primary">Read more on that story here</a>'+
+                          '</div> '+'</div>';
+                }
+            },
+            error:function(error){
+                console.log("Error");
+            }
+
+        });
+
+    }
+})
